2745500e5843675903b88b050e1c2ca679dd99d2
Guilherme Barros - WorkL <hello@gbarros.dev>
2024-03-06 06:51:41 +0000

    FEAT: Add home page, Post component and New Post component

---
+++ src/app/_components/new-post.tsx
@@ -0,0 +1,105 @@
+'use client'
+
+import Image from 'next/image'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { api } from '@/trpc/react'
+import { postFormSchema, type PostFormSchema } from '@/validators/posts'
+
+type NewPostPops = {
+  onRefresh: () => void
+}
+
+export default function NewPost({ onRefresh }: NewPostPops) {
+  const postMutation = api.post.create.useMutation()
+
+  const form = useForm<PostFormSchema>({
+    mode: 'onBlur',
+    reValidateMode: 'onChange',
+    resolver: zodResolver(postFormSchema),
+    defaultValues: {
+      title: '',
+      content: '',
+    },
+  })
+
+  const onSubmit = async (values: PostFormSchema) => {
+    await postMutation.mutateAsync(values, {
+      onSuccess: () => {
+        onRefresh()
+        toast.success('Post created!')
+        form.reset()
+      },
+      onError: (error) => {
+        console.log({ error })
+        toast.error('Failed to create post', { description: error.message })
+      },
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        className='shadow-custom mb-10 min-h-[149px] rounded-xl border border-gray-200'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className='flex p-4'>
+          <Image
+            src='/person-placeholder.png'
+            width={24}
+            height={24}
+            alt='person-placeholder'
+            style={{ objectFit: 'contain' }}
+            className='max-h-[24px]'
+            priority
+          />
+          <div className='ml-4 flex w-full flex-col'>
+            <FormField
+              control={form.control}
+              name='title'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      className='mt-3'
+                      placeholder='Title of your post'
+                      {...field}
+                      value={field?.value ?? ''}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='content'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      className='mt-3 border-b border-gray-200'
+                      placeholder='Share your thoughts with the world!'
+                      {...field}
+                      value={field?.value ?? ''}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className='mt-3 flex w-full flex-row-reverse'>
+              <Button type='submit'>Post</Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </Form>
+  )
+}
---
+++ src/app/_components/post.tsx
@@ -0,0 +1,101 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { useAuth } from '@clerk/nextjs'
+import { DateTime } from 'luxon'
+import { toast } from 'sonner'
+
+import { cn } from '@/lib/utils'
+import { type Post } from '@/server/db/schema'
+import { api } from '@/trpc/react'
+import ChevronDownIcon from '../assets/icons/chevron-down-icon'
+import ChevronUpIcon from '../assets/icons/chevron-up-icon'
+
+type PostProps = {
+  post: Post
+}
+
+export default function PostView({ post }: PostProps) {
+  const { userId } = useAuth()
+
+  const postMinutesDiff = parseInt(
+    DateTime.fromJSDate(post.createdAt).diffNow().toFormat('m').replace('-', ''),
+  )
+  const postHoursDiff = parseInt(DateTime.fromJSDate(post.createdAt).diffNow().toFormat('h').replace('-', ''))
+
+  const votesQuery = api.vote.getAll.useQuery({ postId: post.id })
+  const totalVotes = votesQuery.data?.totalVotes ?? 0
+
+  const currentUserVote = api.vote.getByCurrentUser.useQuery({ postId: post.id }, { enabled: !!userId })
+
+  const createVoteMutation = api.vote.create.useMutation({
+    onSuccess: () => {
+      void votesQuery.refetch()
+      void currentUserVote.refetch()
+    },
+    onError: (error) => {
+      toast.error('An error occurred while voting', { description: error.message })
+    },
+  })
+
+  return (
+    <>
+      <div className='mb-10 flex'>
+        {/* votes */}
+        <div className='flex w-fit flex-col items-center'>
+          <button
+            className={cn(
+              'stroke-color border-none transition-all hover:text-indigo-600',
+              currentUserVote.data?.type === 'up' && 'text-indigo-600',
+            )}
+            disabled={createVoteMutation.isLoading}
+            onClick={(e) => {
+              e.preventDefault()
+              createVoteMutation.mutate({ postId: post.id, commentId: null, type: 'up' })
+            }}
+          >
+            <ChevronUpIcon />
+          </button>
+          <p>{totalVotes}</p>
+          <button
+            className={cn(
+              'stroke-color border-none transition-all hover:text-indigo-600',
+              currentUserVote.data?.type === 'down' && 'text-indigo-600',
+            )}
+            disabled={createVoteMutation.isLoading}
+            onClick={(e) => {
+              e.preventDefault()
+              createVoteMutation.mutate({ postId: post.id, commentId: null, type: 'down' })
+            }}
+          >
+            <ChevronDownIcon />
+          </button>
+        </div>
+        {/* post */}
+        <Link href={`/${post.id}`}>
+          <div className='ml-4'>
+            <div className='flex items-center'>
+              <Image
+                src='/person-placeholder.png'
+                width={24}
+                height={24}
+                alt='person-placeholder'
+                style={{ objectFit: 'contain' }}
+                className='max-h-[24px]'
+                priority
+              />
+              {/* post - createdBy, createdAt */}
+              <h3 className='ml-2 text-sm leading-5 text-gray-600'>
+                Posted by {post.userUsername}{' '}
+                {postMinutesDiff >= 60 ? `${postHoursDiff} hour ago` : `${postMinutesDiff} minutes ago`}
+              </h3>
+            </div>
+            {/* post - title */}
+            <h2 className='mt-[6px] font-medium leading-[14px] text-gray-900'>{post.title}</h2>
+            {/* post - content */}
+            <p className='mt-[6px] text-sm leading-5 text-gray-700'>{post.content}</p>
+          </div>
+        </Link>
+      </div>
+    </>
+  )
+}
