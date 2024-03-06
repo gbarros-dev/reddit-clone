3bab7a6d2d52790cf2b07986df87a665edf0f51b
Guilherme Barros - WorkL <hello@gbarros.dev>
2024-03-06 05:46:47 +0000

    DB: Add schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,34 +1,74 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
+import { createId } from '@paralleldrive/cuid2'
+import { relations, sql, type InferSelectModel } from 'drizzle-orm'
+import { mysqlTableCreator, text, timestamp, varchar } from 'drizzle-orm/mysql-core'
+import { createSelectSchema } from 'drizzle-zod'
 
-import { sql } from "drizzle-orm";
-import {
-  bigint,
-  index,
-  mysqlTableCreator,
-  timestamp,
-  varchar,
-} from "drizzle-orm/mysql-core";
+export const createTable = mysqlTableCreator((name) => `reddit-clone_${name}`)
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = mysqlTableCreator((name) => `reddit-clone_${name}`);
+// posts
 
-export const posts = createTable(
-  "post",
-  {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt").onUpdateNow(),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const postsTable = createTable('posts', {
+  id: varchar('id', { length: 36 }).$defaultFn(createId).primaryKey(),
+  title: text('title'),
+  content: text('content').notNull(),
+  userId: varchar('user_id', { length: 36 }).notNull(),
+  userName: text('user_name').notNull(),
+  userUsername: text('user_username').notNull(),
+  createdAt: timestamp('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp('updatedAt').onUpdateNow(),
+})
+
+export const postTableRelations = relations(postsTable, ({ many }) => ({
+  comments: many(commentsTable),
+}))
+
+export type Post = InferSelectModel<typeof postsTable>
+
+export const postSchema = createSelectSchema(postsTable)
+
+// comments
+
+export const commentsTable = createTable('comments', {
+  id: varchar('id', { length: 36 }).$defaultFn(createId).primaryKey(),
+  content: text('content').notNull(),
+  postId: varchar('post_id', { length: 36 }).notNull(),
+  commentId: varchar('comment_id', { length: 36 }),
+  userId: varchar('user_id', { length: 36 }).notNull(),
+  userName: text('user_name').notNull(),
+  userUsername: text('user_username').notNull(),
+  createdAt: timestamp('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp('updatedAt').onUpdateNow(),
+})
+
+export const commentsTableRelations = relations(commentsTable, ({ one }) => ({
+  post: one(postsTable, {
+    fields: [commentsTable.postId],
+    references: [postsTable.id],
+  }),
+}))
+
+export type Comment = InferSelectModel<typeof commentsTable>
+
+export const commentSchema = createSelectSchema(commentsTable)
+
+// votes
+
+export const votesTable = createTable('votes', {
+  id: varchar('id', { length: 36 }).$defaultFn(createId).primaryKey(),
+  type: text('type', { enum: ['up', 'down'] }).notNull(),
+  postId: varchar('post_id', { length: 36 }),
+  commentId: varchar('comment_id', { length: 36 }),
+  userId: varchar('user_id', { length: 36 }).notNull(),
+  createdAt: timestamp('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp('updatedAt').onUpdateNow(),
+})
+
+export type Vote = InferSelectModel<typeof votesTable>
+
+export const voteSchema = createSelectSchema(votesTable)
