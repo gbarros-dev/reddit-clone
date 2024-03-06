1785b5bcfce2d84863a263ead10a688b70e12782
Guilherme Barros - WorkL <hello@gbarros.dev>
2024-03-06 05:39:55 +0000

    CONFIG: Update Prettier configuration

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,27 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 110,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "^(expo(.*)$)|^(expo$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/utils/(.*)$",
+    "^@/components/(.*)$",
+    "^@/styles/(.*)$",
+    "^@/(.*)$",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.2.2",
 };
 
 export default config;
