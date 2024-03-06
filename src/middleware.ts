919eb50a75a42eb55c78ad1c32e89481e075bfbb
Guilherme Barros - WorkL <hello@gbarros.dev>
2024-03-06 18:35:57 +0000

    FIX: Add ignore routes to Clerk authMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
   publicRoutes: ['/', '/log-in'],
-  // debug: true,
+  ignoredRoutes: ['/((?!api|trpc))(_next.*|.+.[w]+$)'],
 })
 
 export const config = {
