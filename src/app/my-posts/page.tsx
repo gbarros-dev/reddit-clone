f73f3d8623c551b0af079de0032035580b2cbb78
Guilherme Barros - WorkL <hello@gbarros.dev>
2024-03-07 17:04:21 +0000

    FEAT: Add Post empty state

--- src/app/my-posts/page.tsx
+++ src/app/my-posts/page.tsx
@@ -2,9 +2,18 @@
 
 import { api } from '@/trpc/react'
 import Post from '../_components/post'
+import PostEmptyState from '../_components/post-empty-state'
 
 export default function MyPosts() {
   const myPostsQuery = api.post.getAllByUser.useQuery()
 
-  return <div>{myPostsQuery.data?.map((post) => <Post key={post.id} post={post} />)}</div>
+  return (
+    <div>
+      {myPostsQuery.data?.length ? (
+        myPostsQuery.data.map((post) => <Post key={post.id} post={post} />)
+      ) : (
+        <PostEmptyState />
+      )}
+    </div>
+  )
 }
