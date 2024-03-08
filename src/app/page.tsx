0e0a15b1444280e38247f5951c1f8e6b38826827
Guilherme Barros - WorkL <hello@gbarros.dev>
2024-03-08 12:57:46 +0000

    REFACTOR: Remove Feed code from main file and add redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,10 @@
-'use client'
-
-import { Fragment } from 'react'
 import { unstable_noStore as noStore } from 'next/cache'
-import { useAuth } from '@clerk/nextjs'
-
-import { Separator } from '@/components/ui/separator'
-import { api } from '@/trpc/react'
-import NewPost from './_components/new-post'
-import Post from './_components/post'
-import PostEmptyState from './_components/post-empty-state'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
   noStore()
-  const { userId } = useAuth()
 
-  const postsQuery = api.post.getAll.useQuery()
+  redirect('/feed')
 
-  return (
-    <div>
-      {userId ? <NewPost onRefresh={postsQuery.refetch} /> : null}
-
-      {postsQuery.data?.length ? (
-        postsQuery.data.map((post) => (
-          <Fragment key={post.id}>
-            <Post post={post} />
-
-            <Separator className='my-10' />
-          </Fragment>
-        ))
-      ) : (
-        <PostEmptyState />
-      )}
-    </div>
-  )
+  return null
 }
