7424a21469082fe251d39703e61f6529afd50c37
Guilherme Barros - WorkL <hello@gbarros.dev>
2024-03-08 14:33:35 +0000

    FIX: Remove custom flow authentication

--- src/app/(auth)/log-in/page.tsx
+++ src/app/(auth)/log-in/page.tsx
@@ -1,52 +1,28 @@
 'use client'
 
-import { useSignUp } from '@clerk/nextjs'
-import { toast } from 'sonner'
+import { SignInButton } from '@clerk/nextjs'
 
 import GoogleLogo from '../../assets/logo/google-logo'
 
 export default function LogIn() {
-  const { signUp, isLoaded } = useSignUp()
-
-  const handleSignUpWithGoogle = async () => {
-    try {
-      if (!isLoaded) {
-        toast.error('Failed to sign up with Google')
-        return null
-      }
-
-      await signUp.authenticateWithRedirect({
-        strategy: 'oauth_google',
-        redirectUrl: '/sso-callback',
-        redirectUrlComplete: '/',
-      })
-    } catch (error) {
-      console.error('Failed to sign up with Google:', error)
-    }
-  }
-
   return (
     <div className='grid min-h-full place-items-center'>
       <div>
         <h1 className='text-[28px] font-medium leading-10 text-gray-900'>Join the best community ever</h1>
         <h3 className='text-xl leading-8 text-gray-600'>Create an account today</h3>
 
-        <button
-          className='mt-7 flex items-center rounded-xl border border-gray-300 px-5 py-[14px]'
-          onClick={() => handleSignUpWithGoogle()}
-        >
-          <GoogleLogo />
-          <p className='ml-3 text-gray-700'>Continue with Google</p>
-        </button>
+        <SignInButton afterSignInUrl='/' afterSignUpUrl='/' mode='modal'>
+          <button className='mt-7 flex items-center rounded-xl border border-gray-300 px-5 py-[14px]'>
+            <GoogleLogo />
+            <p className='ml-3 text-gray-700'>Continue with Google</p>
+          </button>
+        </SignInButton>
 
         <div className='mt-7 flex'>
           <p className='text-gray-700'>Already have an account?</p>
-          <button
-            className='ml-1 font-medium leading-6 text-[#172554]'
-            onClick={() => handleSignUpWithGoogle()}
-          >
-            Sign-in
-          </button>
+          <SignInButton afterSignInUrl='/' afterSignUpUrl='/' mode='modal'>
+            <button className='ml-1 font-medium leading-6 text-[#172554]'>Sign-in</button>
+          </SignInButton>
         </div>
       </div>
     </div>
